e521ed04712afbc7a31ac0c6ea1fce74c70837ff
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-02-02 01:38:33 +0530

    Added solution - LeetHub

--- Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
+++ Level order traversal in spiral form - GFG/level-order-traversal-in-spiral-form.cpp
@@ -133,43 +133,77 @@ vector<int> findSpiral(Node *root)
     // on 0th level we want R->L and the next level we want L->R and so on..
     
     // Approach1: using two stacks, TC: O(N), SC: O(N)
+    // vector<int> ans;
+    // if(root == NULL) return ans;
+    // stack<Node*> left_to_right; //coz first we want right to left, so take opposite. It stores nodes in order left to right
+    // stack<Node*> right_to_left; //next we want left to right, so take opposite. It stores nodes in order right to left
+    // left_to_right.push(root);
+    
+    // while(left_to_right.size() != 0 || right_to_left.size() != 0) {
+        
+    //     // first R->L
+    //     while(left_to_right.size() != 0) { 
+    //         Node* temp = left_to_right.top();
+    //         left_to_right.pop();
+    //         ans.push_back(temp->data);
+            
+    //         if(temp->right != NULL) {
+    //             right_to_left.push(temp->right);
+    //         }
+    //         if(temp->left != NULL) {
+    //             right_to_left.push(temp->left);
+    //         }
+    //     }
+        
+    //     // then L->R
+    //     while(right_to_left.size() != 0) {
+    //         Node* temp = right_to_left.top();
+    //         right_to_left.pop();
+    //         ans.push_back(temp->data);
+            
+    //         if(temp->left != NULL) {
+    //             left_to_right.push(temp->left);
+    //         }
+    //         if(temp->right != NULL) {
+    //             left_to_right.push(temp->right);
+    //         }
+    //     }
+    // }    
+    
+    // return ans;
+    
+    // Approach 2 => using queue, TC: O(N), SC: O(N)
+    // traverse level order wise and just maintain a flag to change order
     vector<int> ans;
     if(root == NULL) return ans;
-    stack<Node*> left_to_right; //coz first we want right to left, so take opposite. It stores nodes in order left to right
-    stack<Node*> right_to_left; //next we want left to right, so take opposite. It stores nodes in order right to left
-    left_to_right.push(root);
+    queue<Node*> q;
+    q.push(root);
+    bool right_to_left = true;
     
-    while(left_to_right.size() != 0 || right_to_left.size() != 0) {
-        
-        // first R->L
-        while(left_to_right.size() != 0) { 
-            Node* temp = left_to_right.top();
-            left_to_right.pop();
-            ans.push_back(temp->data);
+    while(!q.empty()) {
+        int size = q.size();
+        vector<int> level(size);
+        for(int i=0; i<size; i++) {
+            Node* front = q.front();
+            q.pop();
+            int index = (right_to_left == true) ? size-i-1 : i;
+            level[index] = front->data;
             
-            if(temp->right != NULL) {
-                right_to_left.push(temp->right);
+            if(front->left != NULL) {
+                q.push(front->left);
             }
-            if(temp->left != NULL) {
-                right_to_left.push(temp->left);
+            if(front->right != NULL) {
+                q.push(front->right);
             }
         }
-        
-        // then L->R
-        while(right_to_left.size() != 0) {
-            Node* temp = right_to_left.top();
-            right_to_left.pop();
-            ans.push_back(temp->data);
-            
-            if(temp->left != NULL) {
-                left_to_right.push(temp->left);
-            }
-            if(temp->right != NULL) {
-                left_to_right.push(temp->right);
-            }
+        //curr level is over, so change the bool
+        right_to_left = !right_to_left;
+        for(auto i: level) { 
+            ans.push_back(i);
         }
-    }    
+    }
     
     return ans;
+    
 }
 
