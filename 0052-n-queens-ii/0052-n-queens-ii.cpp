af7c40aa00fe3da71427b0ade93a6dd8d791f815
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-02-28 16:59:13 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (64.87%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -20,21 +20,42 @@ class Solution {
             }
         }
     }
+    
+    int helper(int col, int n, vector<int> &leftRow, vector<int> &leftUpperDiagonal, vector<int> &leftLowerDiagonal) {
+        int count = 0;
+        if(col == n) return 1; //base case
+        
+        for(int row=0; row<n; row++) {
+            if(leftRow[row] == 0 && leftLowerDiagonal[row+col] == 0 && leftUpperDiagonal[n-1 + col-row] == 0) { //it is safe to place the queen
+                // hash all vectors
+                leftRow[row] = 1;
+                leftLowerDiagonal[row+col] = 1;
+                leftUpperDiagonal[n-1 + col-row] = 1;
+                count += helper(col + 1, n, leftRow, leftUpperDiagonal, leftLowerDiagonal); //call rec on next col
+                // unhash all vectors
+                leftRow[row] = 0;
+                leftLowerDiagonal[row+col] = 0;
+                leftUpperDiagonal[n-1 + col-row] = 0;
+            }
+        }
+        
+        return count;
+    }
 
 public:
     int totalNQueens(int n) {
-        // Using hashing, TC: O(N!*N), SC: O(N)
-        vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n, '.');
-        for(int i=0; i<n; i++) {
-            board[i] = s;
-        }
-        int cnt=0;
-        vector<int> leftRow(n,0);
-        vector<int> leftUpperDiagonal(2*n-1,0);
-        vector<int> leftLowerDiagonal(2*n-1, 0);
-        solve(0, n, board, ans, leftRow, leftUpperDiagonal, leftLowerDiagonal, cnt);
+        // Using hashing, TC: O(N!*N), SC: O(N^2) for board and rec stack space
+        // vector<vector<string>> ans;
+        // vector<string> board(n);
+        // string s(n, '.');
+        // for(int i=0; i<n; i++) {
+        //     board[i] = s;
+        // }
+        // int cnt=0;
+        // vector<int> leftRow(n,0);
+        // vector<int> leftUpperDiagonal(2*n-1,0);
+        // vector<int> leftLowerDiagonal(2*n-1, 0);
+        // solve(0, n, board, ans, leftRow, leftUpperDiagonal, leftLowerDiagonal, cnt);
         // for(auto i: ans) {
         //     for(auto j: i) {
         //         for(auto k: j) {
@@ -44,6 +65,14 @@ public:
         //     }
         //     cout<<endl;
         // }
-        return cnt;
+        // return cnt;
+        
+        // We dont need the exact board solutions in this ques, we can remove it
+        // TC: O(N!), SC: O(N)
+        vector<int> leftRow(n,0);
+        vector<int> leftUpperDiagonal(2*n-1,0);
+        vector<int> leftLowerDiagonal(2*n-1, 0);
+        return helper(0, n, leftRow, leftUpperDiagonal, leftLowerDiagonal);
+        
     }
 };
