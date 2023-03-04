c19570aa6ced399b890e27c02e774e7ee8bac03e
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-04 14:47:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (95.02%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int i = 0, j = 0;
+        int m = haystack.size();
+        int k = needle.size(); 
+        int p = 0; //to traverse over needle 
+        
+        while(j < m) {
+            if(haystack[j] == needle[p]) {
+                p++;
+            }
+            if(j - i + 1 < k) {
+                j++;
+            }
+            else if(j - i + 1 == k) { // window size hit
+                if(p == k) return i; // found the word
+                if(p != k) { // the word in this window is not our ans, slide the window
+                    i++;
+                    while(i < m && haystack[i] != needle[0]) {
+                        i++;
+                    }
+                    if(i >= m) return -1; //ans does not exist
+                    j = i;
+                    p = 0;
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
