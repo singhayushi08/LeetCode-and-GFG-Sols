4ea049af905be02d866bc07b370536d7b4d14bee
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-13 01:38:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (19.94%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -14,7 +14,16 @@ public:
         // return climbStairs(n-1) + climbStairs(n-2);
         
         // Memoization, TC: O(N), SC: O(2N) where O(N) is for rec stack space and O(N) for using 1d array 
-        vector<int> dp(n+1, -1);
-        return helper(n, dp);
+        // vector<int> dp(n+1, -1);
+        // return helper(n, dp);
+        
+        // Tabulation, TC: O(N), SC: O(N) only for array
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
