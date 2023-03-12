ecea35568d28c22417d9e3020e4fc85ce69f6614
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-13 01:40:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (95.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -18,12 +18,21 @@ public:
         // return helper(n, dp);
         
         // Tabulation, TC: O(N), SC: O(N) only for array
-        vector<int> dp(n+1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n+1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i=2; i<=n; i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        
+        // Space Optimised, TC: O(N), Sc: O(1)
+        int a = 1, b = 1, c;
         for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            c = a + b;
+            a = b;
+            b = c;
         }
-        return dp[n];
+        return b;
     }
 };
