039441e0d2af9b234faf8bf1a1c743614017b071
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-14 02:33:38 +0530

    Time: 10 ms (35.09%), Space: 13.9 MB (34.16%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -27,10 +27,23 @@ public:
         // return ans;
         
         // Memoization, TC: O(n), SC: O(2n) for array and rec stack
+        // int n = cost.size();
+        // vector<int> dp(n+1, -1);
+        // int ans = min(memo(cost,dp,n-1), memo(cost,dp,n-2));
+        // return ans;
+        
+        // Tabulation, TC: O(n), SC: O(n)
         int n = cost.size();
-        vector<int> dp(n+1, -1);
-        int ans = min(memo(cost,dp,n-1), memo(cost,dp,n-2));
-        return ans;
+        vector<int> dp(n+1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i=2; i<n; i++) {
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        
+        return min(dp[n-1], dp[n-2]);
+        
         
     }
 };
