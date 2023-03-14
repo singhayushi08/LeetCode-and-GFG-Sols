60ce8d84dbc4615205a794ea9b940309e0c803ec
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-14 15:44:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.51%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -23,18 +23,36 @@ public:
         // return trib(n, dp);
         
         // Tabulation, TC: O(n), SC: O(1)
+//         if(n == 0) return 0;
+//         if(n == 1 || n == 2) return 1;
+//         vector<int> dp(n+1);
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         dp[2] = 1;
+        
+//         for(int i=3; i<=n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+//         }
+        
+//         return dp[n];
+        
+        // Space optimised, TC: O(n), SC: O(1)
         if(n == 0) return 0;
         if(n == 1 || n == 2) return 1;
-        vector<int> dp(n+1);
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int prev3 = 0;
+        int prev2 = 1;
+        int prev1 = 1;
         
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int curr = prev1 + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
         
-        return dp[n];
+        return prev1;
+        
+        
         
     }
 };
