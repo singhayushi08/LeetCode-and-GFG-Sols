79baff1222b2f97a7adf7a20c996700ee5fad1ee
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-20 00:14:17 +0530

    Time: 7 ms (31.26%), Space: 7.7 MB (71.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -43,30 +43,58 @@ public:
         // return 0;        
         
         // Tabulation, TC: O(m*n), SC: O(m*n) only for 2d array
+//         if(obstacleGrid[m-1][n-1] == 1) return 0;
+//         vector<vector<int>> dp(m, vector<int>(n));
+//         dp[0][0] = 1;
+        
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(i == 0 && j == 0) {
+//                     dp[i][j] = 1;
+//                 }
+//                 else {
+//                     int up = 0, left = 0;
+//                     if(i-1 >= 0 && obstacleGrid[i-1][j] == 0) {
+//                         up = dp[i-1][j];
+//                     }
+//                     if(j-1 >= 0 && obstacleGrid[i][j-1] == 0) {
+//                         left = dp[i][j-1];
+//                     }
+                    
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        // Space Optimisation, TC: O(m*n), SC: O(n)
         if(obstacleGrid[m-1][n-1] == 1) return 0;
-        vector<vector<int>> dp(m, vector<int>(n));
-        dp[0][0] = 1;
+        vector<int> prev(n,0);
+        prev[0] = 1;
         
         for(int i=0; i<m; i++) {
+            vector<int> curr(n,0);
             for(int j=0; j<n; j++) {
                 if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }
                 else {
                     int up = 0, left = 0;
                     if(i-1 >= 0 && obstacleGrid[i-1][j] == 0) {
-                        up = dp[i-1][j];
+                        up = prev[j];
                     }
                     if(j-1 >= 0 && obstacleGrid[i][j-1] == 0) {
-                        left = dp[i][j-1];
+                        left = curr[j-1];
                     }
                     
-                    dp[i][j] = up + left;
+                    curr[j] = up + left;
                 }
             }
+            prev = curr; // curr will become prev
         }
         
-        return dp[m-1][n-1];
+        return prev[n-1];
         
         
     }
