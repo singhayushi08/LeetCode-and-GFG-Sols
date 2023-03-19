1144f80385c2829aa644dc9be6c7045462bf87b0
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-20 00:11:53 +0530

    Time: 8 ms (14.47%), Space: 7.9 MB (50.48%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -31,15 +31,43 @@ class Solution {
     
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        // Recursion
+        // Recursion, TC: O(2^(m*n)), SC: O(m-1 + n-1 => path length)
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         // if(obstacleGrid[m-1][n-1] == 0) return f(m-1, n-1, obstacleGrid);
         // return 0;
         
-        // Memoization
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        if(obstacleGrid[m-1][n-1] == 0) return memo(m-1, n-1, obstacleGrid, dp);
-        return 0;        
+        // Memoization, TC: O(m*n), SC: O(m*n for 2d array + m-1+n-1 for rec stack space)
+        // vector<vector<int>> dp(m, vector<int>(n,-1));
+        // if(obstacleGrid[m-1][n-1] == 0) return memo(m-1, n-1, obstacleGrid, dp);
+        // return 0;        
+        
+        // Tabulation, TC: O(m*n), SC: O(m*n) only for 2d array
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = 1;
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    int up = 0, left = 0;
+                    if(i-1 >= 0 && obstacleGrid[i-1][j] == 0) {
+                        up = dp[i-1][j];
+                    }
+                    if(j-1 >= 0 && obstacleGrid[i][j-1] == 0) {
+                        left = dp[i][j-1];
+                    }
+                    
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
+        
     }
 };
