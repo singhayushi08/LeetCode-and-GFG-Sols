643e8e64fc10cd5e5ea05ba68b49caa21ecb9396
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-19 22:59:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (63.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -58,24 +58,44 @@ public:
         // return memo(m-1, n-1, dp);
         
         // Tabulation, TC: O(m*n), SC: O(m*n) only for 2d array
-        vector<vector<int>> dp(m, vector<int>(n));
-        dp[0][0] = 1;
+//         vector<vector<int>> dp(m, vector<int>(n));
+//         dp[0][0] = 1;
         
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(i == 0 && j == 0) dp[i][j] = 1;
+//                 else {
+//                     int left = 0, up = 0;
+//                     if(i-1 >= 0) up = dp[i-1][j];
+//                     if(j-1 >= 0) left = dp[i][j-1];
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        // Space Optimisation, TC: O(m*n), SC: O(n)
+        // we only require the prev rows value and prev value in current row to calculate i,j
+        
+        vector<int> prev(n,0); // prev row
+         
         for(int i=0; i<m; i++) {
+            vector<int> curr(n,0); // curr row
             for(int j=0; j<n; j++) {
-                if(i == 0 && j == 0) dp[i][j] = 1;
+                if(i == 0 && j == 0) curr[j] = 1;
                 else {
                     int left = 0, up = 0;
-                    if(i-1 >= 0) up = dp[i-1][j];
-                    if(j-1 >= 0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                    if(i-1 >= 0) up = prev[j];
+                    if(j-1 >= 0) left = curr[j-1];
+                    curr[j] = up + left;
                 }
             }
+            prev = curr; // curr will become prev
         }
         
-        return dp[m-1][n-1];
-        
-        
+        return prev[n-1];
+       
         
         
     }
