0077aac94f89cd8da3a2826fce5c27f2fb95e640
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-20 15:33:43 +0530

    Time: 12 ms (46.87%), Space: 10.1 MB (35.60%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -34,8 +34,29 @@ public:
         // return f(m-1, n-1, grid);
         
         // Memoization, TC: O(m*n), SC: O(m*n + m-1+n-1) for 2d array and rec stack space  
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        return memo(m-1, n-1, grid, dp);
+        // vector<vector<int>> dp(m, vector<int>(n,-1));
+        // return memo(m-1, n-1, grid, dp);
+        
+        // Tabulation, TC: O(m*n), SC: O(m*n)
+        vector<vector<int>> dp(m, vector<int>(n,0));
+        dp[0][0] = grid[0][0];
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i == 0 && j == 0) {
+                    dp[i][j] = grid[0][0];
+                } 
+                else {
+                    int up = INT_MAX, left = INT_MAX;
+                    if(i-1 >= 0) up = dp[i-1][j];
+                    if(j-1 >= 0) left = dp[i][j-1];
+                    
+                    dp[i][j] = grid[i][j] + min(up, left);
+                }
+            } 
+        }
+        
+        return dp[m-1][n-1];
         
         
     }
