e9739950be14bd14fe24fbab7ae78fa7a2e727dc
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-20 15:36:32 +0530

    Time: 16 ms (16.81%), Space: 10 MB (61.11%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -38,26 +38,48 @@ public:
         // return memo(m-1, n-1, grid, dp);
         
         // Tabulation, TC: O(m*n), SC: O(m*n)
-        vector<vector<int>> dp(m, vector<int>(n,0));
-        dp[0][0] = grid[0][0];
+//         vector<vector<int>> dp(m, vector<int>(n,0));
+//         dp[0][0] = grid[0][0];
+        
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(i == 0 && j == 0) {
+//                     dp[i][j] = grid[0][0];
+//                 } 
+//                 else {
+//                     int up = INT_MAX, left = INT_MAX;
+//                     if(i-1 >= 0) up = dp[i-1][j];
+//                     if(j-1 >= 0) left = dp[i][j-1];
+                    
+//                     dp[i][j] = grid[i][j] + min(up, left);
+//                 }
+//             } 
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        // Space Optimisation, TC: O(m*n), SC: O(n)
+        vector<int> prev(n,0);
+        prev[0] = grid[0][0];
         
         for(int i=0; i<m; i++) {
+            vector<int> curr(n,0);
             for(int j=0; j<n; j++) {
                 if(i == 0 && j == 0) {
-                    dp[i][j] = grid[0][0];
+                    curr[j] = grid[0][0];
                 } 
                 else {
                     int up = INT_MAX, left = INT_MAX;
-                    if(i-1 >= 0) up = dp[i-1][j];
-                    if(j-1 >= 0) left = dp[i][j-1];
+                    if(i-1 >= 0) up = prev[j];
+                    if(j-1 >= 0) left = curr[j-1];
                     
-                    dp[i][j] = grid[i][j] + min(up, left);
+                    curr[j] = grid[i][j] + min(up, left);
                 }
             } 
+            prev = curr;
         }
         
-        return dp[m-1][n-1];
-        
+        return prev[n-1];
         
     }
 };
