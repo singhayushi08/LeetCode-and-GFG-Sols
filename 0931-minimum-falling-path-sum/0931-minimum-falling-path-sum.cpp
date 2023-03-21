5cd06934eb23d38cc43b68966d6156e4b42e592f
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-21 17:35:50 +0530

    Time: 18 ms (42.08%), Space: 10.3 MB (61.40%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    int f(int i, int j, int n, vector<vector<int>>& matrix) { //from n-1,n-1 to 0th row
+        if(i == 0) return matrix[0][j]; 
+        
+        int ld = INT_MAX, rd = INT_MAX;
+        int up = matrix[i][j] + f(i-1, j, n, matrix); // row i will never go out of bound but j can
+        
+        if(j-1 >= 0) ld = matrix[i][j] + f(i-1, j-1, n, matrix); // left upper diagonal 
+        if(j+1 < n) rd = matrix[i][j] + f(i-1, j+1, n, matrix); // right upper diagonal
+        
+        return min(up, min(ld, rd));
+    }
+    
+    int memo(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp) { //from n-1,n-1 to 0th row
+        if(i == 0) return matrix[0][j]; 
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int ld = INT_MAX, rd = INT_MAX;
+        int up = matrix[i][j] + memo(i-1, j, n, matrix, dp); // row i will never go out of bound but j can
+        
+        if(j-1 >= 0) ld = matrix[i][j] + memo(i-1, j-1, n, matrix, dp); // left upper diagonal 
+        if(j+1 < n) rd = matrix[i][j] + memo(i-1, j+1, n, matrix, dp); // right upper diagonal
+        
+        return dp[i][j] = min(up, min(ld, rd));
+    }
+    
+    
+   
+    
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // Recursion, TC: O(3^(n*n)), SC: O(n) 
+        // int ans = INT_MAX;
+        // for(int j=0; j<n; j++) {
+        //     ans = min(ans, f(n-1, j, n, matrix));
+        // }
+        // return ans;
+        
+        // Memoization, TC: O(n*n), SC: O(n*n + n) for 2d array and rec stack space
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        int ans = INT_MAX;
+        for(int j=0; j<n; j++) {
+            ans = min(ans, memo(n-1, j, n, matrix, dp));
+        }
+        return ans;
+        
+        
+    }
+};
