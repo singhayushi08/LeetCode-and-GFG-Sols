7ba3ebbf16ebb13b9c4d7a22b0c375de92815784
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-22 00:31:10 +0530

    Time: 957 ms (26.71%), Space: 15.9 MB (42.64%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int f(int i, int last, int n, vector<vector<int>> &grid) { //f(n-1, j) to f(0,j)
+    int f(int i, int last, int n, vector<vector<int>> &grid) { //going from f(n-1, j) to f(0,j)
         if(i == 0) { // base case
             int mini = INT_MAX;
             for(int j=0; j<n; j++) {
@@ -43,16 +43,82 @@ class Solution {
         return dp[i][last] = mini;
     }
     
+    int f(int i, int j, vector<vector<int>>& grid) {
+        if(i == 0) return grid[i][j];
+        
+        int mini = INT_MAX;
+        for(int k=0; k<grid[0].size(); k++) {
+            if(k != j) {
+                int temp = grid[i][j] + f(i-1, k, grid);
+                mini = min(mini, temp);
+            }
+        }
+        
+        return mini;
+    }
+    
+    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp) {
+        if(i == 0) return grid[i][j];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int mini = INT_MAX;
+        for(int k=0; k<grid[0].size(); k++) {
+            if(k != j) {
+                int temp = grid[i][j] + solve(i-1, k, grid, dp);
+                mini = min(mini, temp);
+            }
+        }
+        
+        return dp[i][j] = mini;
+    }
+    
+    
+    
 public:
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
+    int minFallingPathSum(vector<vector<int>>& grid) { 
+        // SIMILAR TO NINJA TRAINING
+        // int n = grid.size();
         
         // Recursion, TC: O(2^n), SC: O(n) for rec tree depth
         // return f(n-1, n, n, grid); //passing nth index as last chosen element, means none taken yet
         
         // Memoization, TC: O(n*n), SC: O(n*n + n) for 2d array and rec stack space
         // max value of i=>n-1 and last=>n
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return memo(n-1, n, n, grid, dp); 
+        // vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // return memo(n-1, n, n, grid, dp);
+        
+        // BETTER APPROACH: VARIABLE STARTING AND ENDING POINT METHOD
+        
+        // OR Recursion
+        // int n = grid.size();
+        // int m = grid[0].size();
+        // int ans = INT_MAX;
+        
+        // make every element of last row as starting point and ending point will be any element on 0th row
+        // for(int j=0; j<m; j++) { 
+        //     ans = min(ans, f(n-1, j, grid));
+        // }
+        // return ans;
+        
+        // Memoization, TC: O(n*m), SC: O(n*m + n) for 2d array and rec stack space
+        int n = grid.size();
+        int m = grid[0].size(); //just in case n and m were different
+        vector<vector<int>> dp(n, vector<int>(m,-1));
+        int ans = INT_MAX;
+        
+        for(int j=0; j<m; j++) {
+            ans = min(ans, solve(n-1, j, grid, dp));
+        }
+        return ans;
+        
+        // Tabulation
+        
+        
+        
+        
+        
+        
+        
     }
 };
