0be8f9d28066d53ff020dd621a5bd5aaacdf5f29
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-22 00:48:01 +0530

    Time: 413 ms (63.63%), Space: 15.8 MB (42.64%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -101,7 +101,7 @@ public:
         // }
         // return ans;
         
-        // Memoization, TC: O(n*m), SC: O(n*m + n) for 2d array and rec stack space
+        // Memoization, TC: O(n*m*m), SC: O(n*m + n) for 2d array and rec stack space
 //         int n = grid.size();
 //         int m = grid[0].size(); //just in case n and m were different
 //         vector<vector<int>> dp(n, vector<int>(m,-1));
@@ -112,32 +112,63 @@ public:
 //         }
 //         return ans;
         
-        // Tabulation
+        // Tabulation, TC: O(n*m*m), SC: O(n*m)
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<vector<int>> dp(n, vector<int>(m,0));
+        
+//         // fill 1st row
+//         for(int j=0; j<m; j++) {
+//             dp[0][j] = grid[0][j];
+//         }
+        
+//         for(int i=1; i<n; i++) {
+//             for(int j=0; j<m; j++) {
+//                 int mini = INT_MAX;
+//                 for(int k=0; k<m; k++) {
+//                     if(k != j) {
+//                         int temp = grid[i][j] + dp[i-1][k];
+//                         mini = min(mini, temp);
+//                     }
+//                 }
+//                 dp[i][j] = mini;
+//             }
+//         }
+        
+//         int ans = dp[n-1][0];
+//         for(int j=1; j<m; j++) {
+//             ans = min(ans, dp[n-1][j]);
+//         }
+//         return ans;
+        
+        // Space Optimisation, TC: O(n*m*m), SC: O(n)
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m,0));
+        vector<int> prev(m,0);
         
         // fill 1st row
         for(int j=0; j<m; j++) {
-            dp[0][j] = grid[0][j];
+            prev[j] = grid[0][j];
         }
         
         for(int i=1; i<n; i++) {
+            vector<int> curr(m,0);
             for(int j=0; j<m; j++) {
                 int mini = INT_MAX;
                 for(int k=0; k<m; k++) {
                     if(k != j) {
-                        int temp = grid[i][j] + dp[i-1][k];
+                        int temp = grid[i][j] + prev[k];
                         mini = min(mini, temp);
                     }
                 }
-                dp[i][j] = mini;
+                curr[j] = mini;
             }
+            prev = curr;
         }
         
-        int ans = dp[n-1][0];
+        int ans = prev[0];
         for(int j=1; j<m; j++) {
-            ans = min(ans, dp[n-1][j]);
+            ans = min(ans, prev[j]);
         }
         return ans;
             
