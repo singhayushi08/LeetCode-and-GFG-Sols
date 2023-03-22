0ddfa94ad1811f3eee02c1c45bf5ff4cd6f49c25
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-03-22 22:47:22 +0530

    Time: 196 ms (31.41%), Space: 9.3 MB (96.60%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -62,12 +62,50 @@ public:
         // return memo(0, 0, m-1, n, m, grid, dp);
         
         // Tabulation, TC: O(n*m*m), SC: O(n*m*m) for 3d dp only
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+//         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        
+//         for(int j1=0; j1<m; j1++) {
+//             for(int j2=0; j2<m; j2++) {
+//                 if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+//                 else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+//             }
+//         }
+        
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j1=0; j1<m; j1++) {
+//                 for(int j2=0; j2<m; j2++) {
+//                     int maxi = -1e8;
+//                     // traverse all paths of both robots simultaneously
+//                     for(int dj1 = -1; dj1 <= 1; dj1++) {
+//                         for(int dj2 = -1; dj2 <= 1; dj2++) {
+//                             int val = 0;
+//                             if(j1 == j2) val = grid[i][j1];
+//                             else val = grid[i][j1] + grid[i][j2];
+                            
+//                             if(dj1+j1 >= 0 && dj1+j1 < m && dj2+j2 >= 0 && dj2+j2 < m)
+//                                 val += dp[i+1][dj1+j1][dj2+j2];
+//                             else 
+//                                 val += -1e8;
+//                             maxi = max(val, maxi);
+//                         }
+//                     }
+
+//                     dp[i][j1][j2] = maxi;
+//                 }
+//             }
+//         }
+        
+//         return dp[0][0][m-1];
+        
+        // Space Optimisation, TC: O(n*m*m), SC: O(m*m)
+        // 1D dp => two variables, 2D dp => 1D, 3D dp => 2D (one dimension less)
+        vector<vector<int>> next(m, vector<int>(m,0));
+        vector<vector<int>> curr(m, vector<int>(m,0));
         
         for(int j1=0; j1<m; j1++) {
             for(int j2=0; j2<m; j2++) {
-                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
-                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                if(j1 == j2) next[j1][j2] = grid[n-1][j1];
+                else next[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
             }
         }
         
@@ -83,19 +121,21 @@ public:
                             else val = grid[i][j1] + grid[i][j2];
                             
                             if(dj1+j1 >= 0 && dj1+j1 < m && dj2+j2 >= 0 && dj2+j2 < m)
-                                val += dp[i+1][dj1+j1][dj2+j2];
+                                val += next[dj1+j1][dj2+j2];
                             else 
                                 val += -1e8;
                             maxi = max(val, maxi);
                         }
                     }
 
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            next = curr; // curr will become next
         }
         
-        return dp[0][0][m-1];
+        return next[0][m-1];
+        
         
         
         
