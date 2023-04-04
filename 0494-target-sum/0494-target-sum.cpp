841342746212a40ae89742a7af46a3a3c6663f9b
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-04 16:21:50 +0530

    Time: 307 ms (37.41%), Space: 55 MB (13.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -46,14 +46,48 @@ class Solution {
         return dp[n-1][k];
     }
     
+    int f(int idx, int currSum, int target, vector<int> &nums) {
+        if(idx == 0) {
+            if((currSum + nums[idx]) == target || (currSum - nums[idx]) == target) return 1;
+            else return 0;
+        }
+        
+        int pos = f(idx-1, currSum + nums[idx], target, nums);
+        int neg = f(idx-1, currSum - nums[idx], target, nums);
+        
+        return pos + neg;
+    }
+    
+     int memo(int idx, int target, vector<int> &nums, map<pair<int,int>,int> &dp) {
+        if(idx == 0) {
+            if(nums[0] == 0 && target == 0) return 2;
+            else return nums[0] == abs(target);
+        }
+         
+        if(dp.find(make_pair(idx,target)) != dp.end()) return dp[make_pair(idx,target)];
+        
+        int pos = memo(idx-1, target + nums[idx], nums, dp);
+        int neg = memo(idx-1, target - nums[idx], nums, dp);
+        
+        return dp[make_pair(idx,target)] = pos + neg;
+    }
     
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         // ques is same as divide set into two subsets s1 and s2 such that s1-s2 = target
-        int totalSum = 0;
-        for(auto i: nums) totalSum += i;
-        if((totalSum - target) < 0 || (totalSum - target)%2 != 0) return 0;
-        int k = (totalSum - target)/2;
-        return countSubsetsWithSumK(nums, k);
+        // int totalSum = 0;
+        // for(auto i: nums) totalSum += i;
+        // if((totalSum - target) < 0 || (totalSum - target)%2 != 0) return 0;
+        // int k = (totalSum - target)/2;
+        // return countSubsetsWithSumK(nums, k);
+        
+        // Rec
+        int n = nums.size();
+        // return f(n-1, 0, target, nums);
+        
+        // Memo
+        map<pair<int,int>,int> dp; //coz -ve values also, cannot use an array
+        return memo(n-1, target, nums, dp);
+        
     }
 };
