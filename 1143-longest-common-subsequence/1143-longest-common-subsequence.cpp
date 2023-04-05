f4af85908eae5ed28b4bab5c6d7df0f7686af10a
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-06 00:54:37 +0530

    Time: 75 ms (28.06%), Space: 24.7 MB (8.31%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int f(int i, int j, string s1, string s2) { 
         //f(i,j) signifies length of longest lcs in s1(0..i) and s2(0..j)  
-        if(i<0 || j<0) return 0;    
+        if(i==0 || j==0) return 0;    
         
-        if(s1[i] == s2[j]) { // if both char match
+        if(s1[i-1] == s2[j-1]) { // if both char match
             return 1 + f(i-1, j-1, s1, s2); // 1 length subsequence found, so add it
         }
         
@@ -12,11 +12,11 @@ class Solution {
     }
     
     int memo(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) { 
-        if(i<0 || j<0) return 0;    
+        if(i==0 || j==0) return 0;    
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s1[i] == s2[j]) { 
+        if(s1[i-1] == s2[j-1]) { 
             return dp[i][j] = 1 + memo(i-1, j-1, s1, s2, dp); 
         }
         
@@ -29,10 +29,15 @@ public:
         int m = text2.size();
         
         // Recursion, TC: O(2^n * 2^m), SC: O(max(n,m))
-        // return f(n-1, m-1, text1, text2);
+        // return f(n, m, text1, text2);
         
-        // Memoization, TC: O(n*m), SC: O(n*m)
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return memo(n-1, m-1, text1, text2, dp);
+        // Memoization, TC: O(n*m), SC: O(n*m) for 2d array + O(n+m) for aux stack space
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return memo(n, m, text1, text2, dp);
+        
+        // Tabulation
+        // vector<vector<int>> dp(n, vector<int>(m+1,0));
+        
+
     }
 };
