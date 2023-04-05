f5ccc95598b4bfac853b4a5d7c13049fa5d05f02
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-06 01:07:49 +0530

    Time: 67 ms (35.50%), Space: 24.4 MB (29.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -32,11 +32,27 @@ public:
         // return f(n, m, text1, text2);
         
         // Memoization, TC: O(n*m), SC: O(n*m) for 2d array + O(n+m) for aux stack space
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return memo(n, m, text1, text2, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        // return memo(n, m, text1, text2, dp);
         
-        // Tabulation
-        // vector<vector<int>> dp(n, vector<int>(m+1,0));
+        // Tabulation, TC: O(n*m), SC: O(n*m)
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for(int j=0; j<=m; j++) { 
+            dp[0][j] = 0;
+        }
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1 + dp[i-1][j-1]; 
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][m];
+        
         
 
     }
