db1e8ea8ecd3d0ccc6772753512d6b7119575399
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-06 22:26:50 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -9,9 +9,9 @@ using namespace std;
 class Solution
 {
     int f(int i, int j, string &s1, string &s2) {
-        if(i < 0 || j < 0) return 0;
+        if(i == 0 || j == 0) return 0;
         
-        if(s1[i] == s2[j]) {
+        if(s1[i-1] == s2[j-1]) {
             return 1 + f(i-1, j-1, s1, s2);
         }    
         
@@ -38,12 +38,25 @@ class Solution
         int m = s2.size();
 
         // Recursion
-        // return f(n-1, m-1, s1, s2);
+        // return f(n, m, s1, s2);
         
         // Memoization
-        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        return memo(n, m, s1, s2, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        // return memo(n, m, s1, s2, dp);
         
+        // Tabulation
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        for(int j=0; j<=m; j++) dp[0][j] = 0;
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return dp[n][m];
     }
 };
 
