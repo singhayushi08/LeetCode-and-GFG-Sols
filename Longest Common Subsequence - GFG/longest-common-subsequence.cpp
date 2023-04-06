013716817c1afa0f065acc60504d046ffe3fcfbc
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-06 22:28:10 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -45,18 +45,32 @@ class Solution
         // return memo(n, m, s1, s2, dp);
         
         // Tabulation
-        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
-        for(int j=0; j<=m; j++) dp[0][j] = 0;
-        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        // for(int j=0; j<=m; j++) dp[0][j] = 0;
+        // for(int i=0; i<=n; i++) dp[i][0] = 0;
+        
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=m; j++) {
+        //         if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+        //         else dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+        //     }
+        // }
+        
+        // return dp[n][m];
+        
+        // Space Optimised
+        vector<int> prev(m+1,0), curr(m+1,0);
+        for(int j=0; j<=m; j++) prev[j] = 0;
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
-                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = 0 + max(dp[i-1][j], dp[i][j-1]);
+                if(s1[i-1] == s2[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = 0 + max(prev[j], curr[j-1]);
             }
+            prev = curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
 
