e0b2bd60168a1e776cead74650ee8dab08f88597
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-10 00:00:36 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+    
+public:
+    string longestPalindrome(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        for(int j=0; j<=m; j++) dp[0][j] = 0;
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        
+        int len = 0, x = 0, y = 0;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    if(dp[i][j] > len && i-dp[i][j] == n-j) {
+                        len = dp[i][j];
+                        x = i;
+                        y = j;
+                        // cout<<"len "<<len<<" updated at x: "<<x<<" , y: "<<y<<endl;
+                    }
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        // for(int i=0; i<=n; i++) {
+        //     for(int j=0; j<=m; j++) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        string res = "";
+        while(dp[x][y] != 0) {
+            res.push_back(s[x-1]);
+            x--;
+            y--;
+        }
+        reverse(res.begin(), res.end());
+        
+        return res;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.longestPalindrome(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
