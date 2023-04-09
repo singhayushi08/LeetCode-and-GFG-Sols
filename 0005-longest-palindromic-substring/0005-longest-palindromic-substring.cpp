7727a2b548e08dcb8d3b85d15b96d6db40808494
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-09 23:15:55 +0530

    Time: 730 ms (17.76%), Space: 284.1 MB (12.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -26,12 +26,12 @@ public:
             }
         }
         
-        // for(int i=0; i<=n; i++) {
-        //     for(int j=0; j<=m; j++) {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
+//         for(int i=0; i<=n; i++) {
+//             for(int j=0; j<=m; j++) {
+//                 cout<<dp[i][j]<<" ";
+//             }
+//             cout<<endl;
+//         }
         
         string res = "";
         while(dp[x][y] != 0) {
