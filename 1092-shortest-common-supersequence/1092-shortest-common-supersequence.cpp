e6859e8ff8847b568ca7a685af8c19bcc9048dd5
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-10 18:54:12 +0530

    Time: 35 ms (23.87%), Space: 12.8 MB (63.52%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -3,11 +3,12 @@ public:
     string shortestCommonSupersequence(string str1, string str2) {
         // keep the Longest common subsequence intact in both the strings
         // then length of shortest common supersequence => n + m - lcs (coz lcs is common in both of them)
+        // print SCS by backtracking LCS
         int n = str1.size();
         int m = str2.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int j=0; j<=m; j++) dp[0][j] = 0;
-        for(int i=0; i<=n; i++) dp[i][0] = 0;
+        // for(int j=0; j<=m; j++) dp[0][j] = 0;
+        // for(int i=0; i<=n; i++) dp[i][0] = 0;
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
@@ -23,27 +24,27 @@ public:
         string ans = "";
         while(i > 0 && j > 0) {
             if(str1[i-1] == str2[j-1]) {
-                ans.push_back(str1[i-1]);
+                ans += str1[i-1]; // or use ans.push_back(str1[i-1])
                 i--;
                 j--;
             } 
             else if(dp[i-1][j] > dp[i][j-1]) { // go up
-                ans.push_back(str1[i-1]);
+                ans += str1[i-1];
                 i--;
             } 
             else { // go left
-                ans.push_back(str2[j-1]); 
+                ans += str2[j-1]; 
                 j--;
             }
         }
         
         // add if characters remaining in either of the str
         while(i > 0 ) {
-            ans.push_back(str1[i-1]);
+            ans += str1[i-1];
             i--;
         }
         while(j > 0) {
-            ans.push_back(str2[j-1]);
+            ans += str2[j-1];
             j--;
         }
         
