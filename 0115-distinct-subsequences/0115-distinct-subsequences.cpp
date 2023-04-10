a7192bfc2b9b8b34138313bcfe8c32aabf040c92
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-11 00:02:31 +0530

    Time: 58 ms (37.75%), Space: 30.1 MB (43.60%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,9 +1,9 @@
 class Solution {
     int f(int i, int j, string &s, string &t) {
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
         
-        if(s[i] == t[j]) {
+        if(s[i-1] == t[j-1]) {
             return f(i-1, j-1, s, t) + f(i-1, j, s, t);
         }
         
@@ -11,11 +11,11 @@ class Solution {
     }
     
     int memo(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+        if(j == 0) return 1;
+        if(i == 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s[i] == t[j]) {
+        if(s[i-1] == t[j-1]) {
             return dp[i][j] = memo(i-1, j-1, s, t, dp) + memo(i-1, j, s, t, dp);
         }
         
@@ -24,14 +24,32 @@ class Solution {
     
 public:
     int numDistinct(string s, string t) {
+        // following 1 based indexing just like in lcs
         int n = s.size();
         int m = t.size();
         
         // Recursion
-        // return f(n-1, m-1, s,t);
+        // return f(n, m, s,t);
         
         // Memoization
-        vector<vector<int>> dp(n, vector<int>(m,-1));
-        return memo(n-1, m-1, s, t, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        // return memo(n, m, s, t, dp);
+        
+        // Tabulation
+        vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+        
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[n][m];
+
     }
 };
