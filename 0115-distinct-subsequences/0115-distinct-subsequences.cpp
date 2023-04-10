7978b7075c7c289054ba644659cbe431f67b58cf
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-11 00:05:03 +0530

    Time: 32 ms (83.91%), Space: 6.5 MB (84.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -36,20 +36,36 @@ public:
         // return memo(n, m, s, t, dp);
         
         // Tabulation
-        vector<vector<double>> dp(n+1, vector<double>(m+1,0));
-        for(int i=0; i<=n; i++) dp[i][0] = 1;
+//         vector<vector<double>> dp(n+1, vector<double>(m+1,0));
+//         for(int i=0; i<=n; i++) dp[i][0] = 1;
+        
+//         for(int i=1; i<=n; i++) {
+//             for(int j=1; j<=m; j++) {
+//                 if(s[i-1] == t[j-1]) {
+//                     dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+//                 }
+        
+//                 else dp[i][j] = dp[i-1][j];
+//             }
+//         }
+        
+//         return dp[n][m];
+        
+        // Space Optimised
+        vector<double> prev(m+1, 0), curr(m+1, 0);
+        prev[0] = curr[0] = 1;
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
                 if(s[i-1] == t[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 }
-        
-                else dp[i][j] = dp[i-1][j];
+                else curr[j] = prev[j];
             }
+            prev = curr;
         }
         
-        return dp[n][m];
+        return curr[m];
 
     }
 };
