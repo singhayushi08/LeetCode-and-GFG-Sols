c0b545ad0713ef06beeac9c13832f1ebdd16e343
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-11 23:32:41 +0530

    Time: 3 ms (99.19%), Space: 6.5 MB (94.40%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,13 +14,13 @@ class Solution {
         return min(a, min(b,c));
     }
     
-    int memo(int i, int j, string s1, string s2, vector<vector<int>> &dp) {
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
+    int memo(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(i == 0) return j;
+        if(j == 0) return i;
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s1[i] == s2[j]) {
+        if(s1[i-1] == s2[j-1]) {
             return dp[i][j] = 0 + memo(i-1, j-1, s1, s2, dp);
         }
         
@@ -31,16 +31,67 @@ class Solution {
         
         return dp[i][j] = min(a, min(b,c));
     }
+    
 public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
         
-        // Recursion
-        // return f(n-1, m-1, word1, word2);
+        // Recursion, TC: expontential, SC: O(n+m)
+        // return f(n, m, word1, word2);
         
-        // Memoization
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return memo(n-1, m-1, word1, word2, dp);
+        // Memoization, TC: O(n*m), SC: O(n*m + n+m) for 2d array and rec stack space
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return memo(n, m, word1, word2, dp);
+        
+        // Tabulation, TC: O(n*m), SC: O(n*m)
+//         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+//         for(int j=0; j<=m; j++) dp[0][j] = j;
+//         for(int i=0; i<=n; i++) dp[i][0] = i;
+        
+//         for(int i=1; i<=n; i++) {
+//             for(int j=1; j<=m; j++) {
+//                 if(word1[i-1] == word2[j-1]) {
+//                     dp[i][j] = 0 + dp[i-1][j-1];
+//                 }
+//                 else {
+//                     int a = 1 + dp[i][j-1]; // insert
+//                     int b = 1 + dp[i-1][j]; // delete
+//                     int c = 1 + dp[i-1][j-1]; // replace
+
+//                     dp[i][j] = min(a, min(b,c));    
+//                 }   
+//             }
+//         }
+        
+//         return dp[n][m];
+        
+        // Space Optimised, TC: O(n*m), SC: O(m)
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        for(int j=0; j<=m; j++) prev[j] = j;
+         
+        for(int i=1; i<=n; i++) {
+            curr[0] = i;
+            for(int j=1; j<=m; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    curr[j] = 0 + prev[j-1];
+                }
+                else {
+                    int a = 1 + curr[j-1]; // insert
+                    int b = 1 + prev[j]; // delete
+                    int c = 1 + prev[j-1]; // replace
+
+                    curr[j] = min(a, min(b,c));    
+                }   
+            }
+            prev = curr;
+        }
+        
+        return prev[m];
+        
+        
+
+        
+        
     }
 };
