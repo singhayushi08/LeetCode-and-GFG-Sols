8d4692c6e02d85cedf3f6b2fa9dab572686d0ae0
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-12 21:37:44 +0530

    Added solution - LeetHub

---
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    bool f(int i, int j, string &s1, string &s2) { //convert into 1 based indxing for tabulation
+        if(i < 0 && j < 0) return true;
+        if(i < 0 && j >= 0) return false;
+        if(j < 0 && i >= 0) {
+            for(int k=0; k<=i; k++) {
+                if(s1[k] != '*') return false;
+            }
+            return true;
+        }
+        
+        if(s1[i] == s2[j] || s1[i] == '?') {
+            return f(i-1, j-1, s1, s2);
+        }
+        else if(s1[i] == '*') {
+            return f(i-1, j, s1, s2) || f(i, j-1, s1, s2);
+        }
+        else {
+            return false;
+        }
+    }
+    
+    bool memo(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(i == 0 && j == 0) return true;
+        if(i == 0 && j > 0) return false;
+        if(j == 0 && i > 0) {
+            for(int k=1; k<=i; k++) {
+                if(s1[k-1] != '*') return false;
+            }
+            return true;
+        }
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(s1[i-1] == s2[j-1] || s1[i-1] == '?') {
+            return dp[i][j] = memo(i-1, j-1, s1, s2, dp);
+        }
+        else if(s1[i-1] == '*') {
+            return dp[i][j] = memo(i-1, j, s1, s2, dp) || memo(i, j-1, s1, s2, dp);
+        }
+        else {
+            return dp[i][j] = false;
+        }
+    }
+    
+  public:
+/*You are required to complete this method*/
+    int wildCard(string pattern,string str)
+    {
+        int n = pattern.size();
+        int m = str.size();
+        // return f(n-1, m-1, pattern, str);
+        
+        // Memoization
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1)); 
+        return memo(n, m, pattern, str, dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--)
+   {
+           string pat,text;
+           cin>>pat;
+cin.ignore(numeric_limits<streamsize>::max(),'\n');
+           cin>>text;
+           Solution obj;
+           cout<<obj.wildCard(pat,text)<<endl;
+   }
+}
+
+// } Driver Code Ends
