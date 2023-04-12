4a8ffc3b1e1c832c1b0d0ee4cac575d3567ebfe3
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-12 14:47:02 +0530

    Time: 90 ms (35.37%), Space: 11.3 MB (54.48%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -20,21 +20,21 @@ class Solution {
     }
     
     bool memo(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
-        if(i < 0 && j < 0) return true;
-        if(j < 0 && i >= 0) return false;
-        if(i < 0 && j >= 0) {
-            for(int k=0; k<=j; k++) {
-                if(p[k] != '*') return false;
+        if(i == 0 && j == 0) return true;
+        if(j == 0 && i > 0) return false;
+        if(i == 0 && j > 0) {
+            for(int k=1; k<=j; k++) {
+                if(p[k-1] != '*') return false;
             }
             return true;
         }
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s[i] == p[j] || p[j] == '?') {
+        if(s[i-1] == p[j-1] || p[j-1] == '?') {
             return dp[i][j] = memo(i-1, j-1, s, p, dp);
         } 
-        if(p[j] == '*') {
+        if(p[j-1] == '*') {
             return dp[i][j] = memo(i, j-1, s, p, dp) || memo(i-1, j, s, p, dp);
         }
         
@@ -50,7 +50,41 @@ public:
         // return f(n-1, m-1, s, p);
         
         // Memoization
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return memo(n-1, m-1, s, p, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        // return memo(n, m, s, p, dp);
+        
+        // Tabulation
+        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
+        dp[0][0] = true;
+        for(int i=1; i<=n; i++) {
+            dp[i][0] = false;
+        }
+        for(int j=1; j<=m; j++) {
+            bool flag = true;
+            for(int k=1; k<=j; k++) {
+                if(p[k-1] != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=m; j++) {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } 
+                else if(p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+
+                else dp[i][j] = false;
+            }
+        }
+        
+        return dp[n][m];
+
+        
     }
 };
