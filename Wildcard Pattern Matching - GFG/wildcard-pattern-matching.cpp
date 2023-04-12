964e71c3ca21f0d9b65c3793dc8b0b2cdf3bff8c
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-12 21:44:32 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -61,9 +61,41 @@ class Solution{
         // return memo(n, m, pattern, str, dp);
         
         // Tabulation
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        dp[0][0] = true;
-        for(int j=1; j<=m; j++) dp[0][j] = false;
+        // vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // dp[0][0] = true;
+        // for(int j=1; j<=m; j++) dp[0][j] = false;
+        // for(int i=1; i<=n; i++) {
+        //     bool flag = true;
+        //     for(int k=1; k<=i; k++) {
+        //         if(pattern[k-1] != '*') {
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     dp[i][0] = flag;
+        // }
+        
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=m; j++) {
+        //         if(pattern[i-1] == str[j-1] || pattern[i-1] == '?') {
+        //             dp[i][j] = dp[i-1][j-1];
+        //         }
+        //         else if(pattern[i-1] == '*') {
+        //             dp[i][j] = dp[i-1][j] || dp[i][j-1];
+        //         }
+        //         else {
+        //             dp[i][j] = false;
+        //         }
+        //     }
+        // }
+        
+        // return dp[n][m];
+        
+        // Space optimisation
+        vector<int> prev(m+1, 0), curr(m+1, 0);
+        prev[0] = true;
+        for(int j=1; j<=m; j++) prev[j] = false;
+        
         for(int i=1; i<=n; i++) {
             bool flag = true;
             for(int k=1; k<=i; k++) {
@@ -72,24 +104,22 @@ class Solution{
                     break;
                 }
             }
-            dp[i][0] = flag;
-        }
-        
-        for(int i=1; i<=n; i++) {
+            curr[0] = flag;
             for(int j=1; j<=m; j++) {
                 if(pattern[i-1] == str[j-1] || pattern[i-1] == '?') {
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
                 else if(pattern[i-1] == '*') {
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                    curr[j] = prev[j] || curr[j-1];
                 }
                 else {
-                    dp[i][j] = false;
+                    curr[j] = false;
                 }
             }
+            prev = curr;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
 
