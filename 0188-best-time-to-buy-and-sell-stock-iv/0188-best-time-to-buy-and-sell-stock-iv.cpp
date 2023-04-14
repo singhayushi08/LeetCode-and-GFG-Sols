b7ba98b32a6c6765c9d1d302d61abee6983eb099
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-15 00:12:27 +0530

    Time: 16 ms (69.74%), Space: 10.9 MB (94.94%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -23,25 +23,48 @@ public:
         // return f(0, 1, k, prices,dp); 
         
         // Tabulation
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2,vector<int>(k+1,0)));
+//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2,vector<int>(k+1,0)));
+        
+//         for(int idx=n-1; idx>=0; idx--) {
+//             for(int buy=0; buy<=1; buy++) {
+//                 for(int cap=k; cap>0; cap--) {
+//                     int profit = 0;
+//                     if(buy == 1) { 
+//                         profit = max(-prices[idx]+ dp[idx+1][0][cap], 0 + dp[idx+1][1][cap]);
+//                     }
+//                     else { 
+//                         profit = max(prices[idx]+ dp[idx+1][1][cap-1], 0 + dp[idx+1][0][cap]);
+//                     }
+
+//                     dp[idx][buy][cap] = profit;
+//                 }
+//             }
+//         }
+        
+//         return dp[0][1][k];
+        
+        // Space Optimised
+        vector<vector<int>> next(2,vector<int>(k+1,0));
+         vector<vector<int>> curr(2,vector<int>(k+1,0));
         
         for(int idx=n-1; idx>=0; idx--) {
             for(int buy=0; buy<=1; buy++) {
                 for(int cap=k; cap>0; cap--) {
                     int profit = 0;
                     if(buy == 1) { 
-                        profit = max(-prices[idx]+ dp[idx+1][0][cap], 0 + dp[idx+1][1][cap]);
+                        profit = max(-prices[idx]+ next[0][cap], 0 + next[1][cap]);
                     }
                     else { 
-                        profit = max(prices[idx]+ dp[idx+1][1][cap-1], 0 + dp[idx+1][0][cap]);
+                        profit = max(prices[idx]+ next[1][cap-1], 0 + next[0][cap]);
                     }
 
-                    dp[idx][buy][cap] = profit;
+                    curr[buy][cap] = profit;
                 }
             }
+            next = curr;
         }
         
-        return dp[0][1][k];
+        return next[1][k];
 
     }
 };
