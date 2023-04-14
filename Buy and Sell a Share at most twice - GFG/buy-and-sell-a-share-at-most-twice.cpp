bddbbb3b145b2a14a0335ba08441101edd281a56
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-14 23:25:48 +0530

    Added solution - LeetHub

--- Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
+++ Buy and Sell a Share at most twice - GFG/buy-and-sell-a-share-at-most-twice.cpp
@@ -52,8 +52,37 @@ int maxProfit(vector<int>&price){
     
     // Memo
     int n = price.size();
-    vector<vector<int>> dp(n, vector<int>(4,-1));
-    return memo(0, 0, price, dp);
+    // vector<vector<int>> dp(n, vector<int>(4,-1));
+    // return memo(0, 0, price, dp);
+    
+    // Tabulation
+    vector<vector<int>> dp(n+1, vector<int>(5,0));
+    for(int t=0; t<=4; t++) {
+        dp[n][t] = 0;
+    }
+    for(int idx=0; idx<=n; idx++) {
+        dp[idx][4] = 0;
+    }
+    
+    for(int idx=n-1; idx>=0; idx--) {
+        for(int transNo=3; transNo>=0; transNo--) {
+            int profit = 0;
+            if(transNo % 2 == 0 ) { // buy
+                int buyy = -price[idx] + memo(idx+1, transNo+1, price, dp);
+                int notBuy = 0 + memo(idx+1, transNo, price, dp);
+                profit = max(buyy, notBuy);
+            } 
+            else { //sell
+                int sell = price[idx] + memo(idx+1, transNo+1, price, dp);
+                int notSell = 0 + memo(idx+1, transNo, price, dp);
+                profit = max(sell, notSell);
+            } 
+            dp[idx][transNo] = profit;
+        }
+    }
+    
+    return dp[0][0];
+
     
 }
 
