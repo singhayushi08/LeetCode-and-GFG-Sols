3194333e40d6fee643809d6df5b0f3d85c059e66
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-15 00:35:15 +0530

    Time: 11 ms (85.81%), Space: 11 MB (78.31%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -44,27 +44,83 @@ public:
 //         return dp[0][1][k];
         
         // Space Optimised
-        vector<vector<int>> next(2,vector<int>(k+1,0));
-         vector<vector<int>> curr(2,vector<int>(k+1,0));
+//         vector<vector<int>> next(2,vector<int>(k+1,0));
+//          vector<vector<int>> curr(2,vector<int>(k+1,0));
+        
+//         for(int idx=n-1; idx>=0; idx--) {
+//             for(int buy=0; buy<=1; buy++) {
+//                 for(int cap=k; cap>0; cap--) {
+//                     int profit = 0;
+//                     if(buy == 1) { 
+//                         profit = max(-prices[idx]+ next[0][cap], 0 + next[1][cap]);
+//                     }
+//                     else { 
+//                         profit = max(prices[idx]+ next[1][cap-1], 0 + next[0][cap]);
+//                     }
+
+//                     curr[buy][cap] = profit;
+//                 }
+//             }
+//             next = curr;
+//         }
+        
+//         return next[1][k];
+        
+        // ANOTHER WAY
+        // if K = 2, 
+        // 0 1 2 3 => transaction No.
+        // B S B S => buy and sell, buy and sell (total = 2*K = 4)
+        
+        // Memo, TC: O(N*K), SC: O(N*K + N)
+        // vector<vector<int>> dp(N, vector<int>(2*K, -1));
+        // return memo(0, 0, k, prices, dp);
+        
+        // Tabulation, TC: O(N*K), SC: O(N*K)
+//         vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+        
+//         for(int idx=n-1; idx>=0; idx--) {
+//             for(int transNo=2*k-1; transNo>=0; transNo--) {
+//                 int profit = 0;
+//                 if(transNo % 2 == 0) { // buy
+//                     int buyy = -prices[idx] + dp[idx+1][transNo+1];
+//                     int notBuy = 0 + dp[idx+1][transNo];
+//                     profit = max(buyy, notBuy);
+//                 } 
+//                 else { // sell
+//                     int sell = prices[idx] + dp[idx+1][transNo+1];
+//                     int notSell = 0 + dp[idx+1][transNo];
+//                     profit = max(sell, notSell);
+//                 }
+                
+//                 dp[idx][transNo] = profit;
+//             }
+//         }
+        
+//         return dp[0][0];
+        
+        // Space Optimised, TC: O(N*K), SC: O(2*K)
+        vector<int> next(2*k+1, 0), curr(2*k+1, 0);
         
         for(int idx=n-1; idx>=0; idx--) {
-            for(int buy=0; buy<=1; buy++) {
-                for(int cap=k; cap>0; cap--) {
-                    int profit = 0;
-                    if(buy == 1) { 
-                        profit = max(-prices[idx]+ next[0][cap], 0 + next[1][cap]);
-                    }
-                    else { 
-                        profit = max(prices[idx]+ next[1][cap-1], 0 + next[0][cap]);
-                    }
-
-                    curr[buy][cap] = profit;
+            for(int transNo=2*k-1; transNo>=0; transNo--) {
+                int profit = 0;
+                if(transNo % 2 == 0) { // buy
+                    int buyy = -prices[idx] + next[transNo+1];
+                    int notBuy = 0 + next[transNo];
+                    profit = max(buyy, notBuy);
+                } 
+                else { // sell
+                    int sell = prices[idx] + next[transNo+1];
+                    int notSell = 0 + next[transNo];
+                    profit = max(sell, notSell);
                 }
+                
+                curr[transNo] = profit;
             }
             next = curr;
         }
         
-        return next[1][k];
+        return next[0];
 
     }
 };
