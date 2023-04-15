4fdc2d47eac1ad14776c2ccf080eb73a868ed5a4
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-15 17:21:31 +0530

    Time: 3 ms (86.13%), Space: 11.9 MB (57.08%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -19,7 +19,46 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return f(0,1,prices, dp);
+        
+        // Rec + memo
+        // vector<vector<int>> dp(n, vector<int>(2,-1));
+        // return f(0,1,prices, dp);
+        
+        // Tabulation
+//         vector<vector<int>> dp(n+2, vector<int>(2,0));
+        
+//         for(int idx=n-1; idx>=0; idx--) {
+//             for(int buy=0; buy<=1; buy++) { 
+//                 if(buy == 1) {
+//                     int buyy = -prices[idx] + dp[idx+1][0];
+//                     int notBuy = 0 + dp[idx+1][1];
+//                     dp[idx][buy] = max(buyy, notBuy);
+//                 } 
+//                 else {
+//                     int sell = prices[idx] + dp[idx+2][1];
+//                     int notSell = 0 + dp[idx+1][0];
+//                     dp[idx][buy] = max(sell, notSell);
+//                 }
+//             }
+//         }
+        
+//         return dp[0][1];
+        
+        // you can remove the buy loop and execute both buy=0, buy=1 at once
+        vector<vector<int>> dp(n+2, vector<int>(2,0));
+        
+        for(int idx=n-1; idx>=0; idx--) {
+            int buyy = -prices[idx] + dp[idx+1][0];
+            int notBuy = 0 + dp[idx+1][1];
+            dp[idx][1] = max(buyy, notBuy);
+
+
+            int sell = prices[idx] + dp[idx+2][1];
+            int notSell = 0 + dp[idx+1][0];
+            dp[idx][0] = max(sell, notSell);
+        }
+        
+        return dp[0][1];
+
     }
 };
