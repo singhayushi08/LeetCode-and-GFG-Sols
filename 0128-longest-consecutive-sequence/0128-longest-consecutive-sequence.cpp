48c2637e6940dbfabfc6793770b492e21b786901
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-16 00:28:00 +0530

    Time: 132 ms (72.35%), Space: 49.6 MB (64.96%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -22,23 +22,44 @@ public:
         // return ans;
         
         // Better, TC: O(nlogn), SC: O(1)
-        int n = nums.size();
-        if(n == 0 || n == 1) return n;
-        sort(nums.begin(), nums.end());
-        int prev = nums[0], ans = 1;
-        int curr_len = 1;
-        for(int i=1; i<n; i++) {
-            if(nums[i] == prev + 1) {
-                curr_len += 1;
-                prev = nums[i];
-                ans = max(curr_len, ans);
-            }
-            else if(nums[i] == prev) {
-                continue;
-            }
-            else if(nums[i] != prev + 1) {
-                curr_len = 1; //reset 
-                prev = nums[i];
+//         int n = nums.size();
+//         if(n == 0 || n == 1) return n;
+//         sort(nums.begin(), nums.end());
+//         int prev = nums[0], ans = 1;
+//         int curr_len = 1;
+//         for(int i=1; i<n; i++) {
+//             if(nums[i] == prev + 1) {
+//                 curr_len += 1;
+//                 prev = nums[i];
+//                 ans = max(curr_len, ans);
+//             }
+//             else if(nums[i] == prev) {
+//                 continue;
+//             }
+//             else if(nums[i] != prev + 1) { // 1 2 3 4 100 101 102
+//                 curr_len = 1; //reset 
+//                 prev = nums[i];
+//             }
+//         }
+        
+//         return ans;
+        
+        // Using set, TC: O(n), SC: O(n) avoiding collision
+        unordered_set<int> s;
+        for(int i=0; i<nums.size(); i++) {
+            s.insert(nums[i]);
+        }
+        
+        int ans = 0;
+        for(auto i: s) {
+            int curr = i;
+            if(s.find(curr-1) == s.end()) {
+                int curr_len = 1;
+                while(s.find(curr+1) != s.end()) {
+                    curr += 1;
+                    curr_len += 1;
+                }
+                ans = max(ans, curr_len);
             }
         }
         
