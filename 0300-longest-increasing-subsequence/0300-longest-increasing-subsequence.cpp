6cf50da34d540a0250a3f870f4ff3a34b4ee0d1e
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-16 22:35:12 +0530

    Time: 491 ms (32.68%), Space: 10.7 MB (40.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
     int f(int idx, int prev_idx, vector<int>& nums, vector<vector<int>> &dp) { // f(0,-1) signifies max length of lis starting from idx 0 and previdx as -1
         if(idx == nums.size()) return 0;    
-        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1];
+        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx+1]; 
         
         int notTake = 0 + f(idx+1, prev_idx, nums, dp);
         int take = 0;
@@ -16,7 +16,42 @@ public:
     int lengthOfLIS(vector<int>& nums) {        
         // Memoization
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return f(0, -1, nums, dp);
+        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // return f(0, -1, nums, dp);
+        
+        // Tabulation
+//         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+//         for(int idx=n-1; idx>=0; idx--) {
+//             for(int prev_idx=idx-1; prev_idx>=-1; prev_idx--) {
+//                 int notTake = 0 + dp[idx+1][prev_idx+1];
+//                 int take = 0;
+//                 if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+//                     take = 1 + dp[idx+1][idx+1];
+//                 }
+
+//                 dp[idx][prev_idx+1] = max(take, notTake);
+//             }
+//         }
+
+//         return dp[0][-1+1];
+        
+        // Space Optimised
+        vector<int> next(n+1, 0), curr(n+1, 0);
+        
+        for(int idx=n-1; idx>=0; idx--) {
+            for(int prev_idx=idx-1; prev_idx>=-1; prev_idx--) {
+                int notTake = 0 + next[prev_idx+1];
+                int take = 0;
+                if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+                    take = 1 + next[idx+1];
+                }
+
+                curr[prev_idx+1] = max(take, notTake);
+            }
+            next = curr;
+        }
+
+        return next[-1+1];
     }
 };
