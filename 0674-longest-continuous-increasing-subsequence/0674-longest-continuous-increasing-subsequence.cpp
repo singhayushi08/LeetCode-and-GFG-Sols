a56f42fd46d32d3279dbbe1640cd1b0269506442
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-21 00:01:32 +0530

    Time: 12 ms (41.24%), Space: 11.1 MB (20.07%) - LeetHub

--- 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
     int findLengthOfLCIS(vector<int>& nums) {
+        // Using DP, variation of Longest increasing subsequence, TC: O(n), SC: O(n)
+        // int n = nums.size();
+        // if(n == 1) return 1;
+        
+//         vector<int> dp(n, 1);
+//         int maxi = 1;
+        
+//         for(int i=1; i<n; i++) {
+//             if(nums[i] > nums[i-1] && dp[i] < 1 + dp[i-1]) {
+//                 dp[i] = 1 + dp[i-1];
+//             }
+//             maxi = max(maxi, dp[i]);
+//         }
+        
+        
+//         return maxi;
+        
+        // Using kadane's algo, TC: O(n), SC: O(1)
         int n = nums.size();
         if(n == 1) return 1;
-        
-        vector<int> dp(n, 1);
-        int maxi = 1;
+        int currLen = 1, maxLen = 1;
         
         for(int i=1; i<n; i++) {
-            if(nums[i] > nums[i-1] && dp[i] < 1 + dp[i-1]) {
-                dp[i] = 1 + dp[i-1];
+           if(nums[i] > nums[i-1]) {
+                currLen++;
             }
-            maxi = max(maxi, dp[i]);
+            else {
+                maxLen = max(maxLen, currLen);
+                currLen = 1; // reset
+            } 
         }
         
-        
-        return maxi;
+        maxLen = max(maxLen, currLen);
+        return maxLen;
     }
 };
