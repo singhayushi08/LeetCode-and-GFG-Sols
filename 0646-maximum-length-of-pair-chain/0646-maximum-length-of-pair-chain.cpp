4e3c7d5453d87895438fa51b816f57fe9638a043
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-21 02:54:07 +0530

    Time: 698 ms (10.97%), Space: 159.1 MB (8.46%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,20 +1,55 @@
 class Solution {
+    int f(int idx, int prev, vector<vector<int>>& pairs) {
+        if(idx == pairs.size()) return 0;
+        int notPick = 0 + f(idx+1, prev, pairs);
+        int pick = INT_MIN;
+        if(prev == -1 || pairs[idx][0] > pairs[prev][1]) {
+            pick = 1 + f(idx+1, idx, pairs);
+        }
+        return max(pick, notPick);
+    }
+    
+    int memo(int idx, int prev, vector<vector<int>>& pairs, vector<vector<int>> &dp) {
+        if(idx == pairs.size()) return 0;
+        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
+        
+        int notPick = 0 + memo(idx+1, prev, pairs, dp);
+        int pick = INT_MIN;
+        if(prev == -1 || pairs[idx][0] > pairs[prev][1]) {
+            pick = 1 + memo(idx+1, idx, pairs, dp);
+        }
+        return  dp[idx][prev+1] = max(pick, notPick);
+    }
+    
 public:
     int findLongestChain(vector<vector<int>>& pairs) { // Variation of LIS
-        sort(pairs.begin(), pairs.end());
+        // BEST: using DP, TC: O(n^2), SC: O(n)
+//         sort(pairs.begin(), pairs.end());
         
+//         int n = pairs.size();
+//         vector<int> dp(n, 1);
+//         int maxi = 1;
+//         for(int i=0; i<n; i++) {
+//             for(int prev=0; prev<i; prev++) {
+//                 if(pairs[i][0] > pairs[prev][1] && dp[i] < 1 + dp[prev]) { 
+//                     dp[i] = 1 + dp[prev];   
+//                 }
+//             }
+//             maxi = max(maxi, dp[i]);
+//         }
+        
+//         return maxi;
+        
+        // Pure LIS Recursion
+        // sort(pairs.begin(), pairs.end());
+        // return f(0, -1, pairs);
+        
+        // Memoization, TC: O(n^2), SC: O(n^2)
         int n = pairs.size();
-        vector<int> dp(n, 1);
-        int maxi = 1;
-        for(int i=0; i<n; i++) {
-            for(int prev=0; prev<i; prev++) {
-                if(pairs[i][0] > pairs[prev][1] && dp[i] < 1 + dp[prev]) { 
-                    dp[i] = 1 + dp[prev];   
-                }
-            }
-            maxi = max(maxi, dp[i]);
-        }
+        sort(pairs.begin(), pairs.end());
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return memo(0, -1, pairs, dp);
+
         
-        return maxi;
     }
 };
