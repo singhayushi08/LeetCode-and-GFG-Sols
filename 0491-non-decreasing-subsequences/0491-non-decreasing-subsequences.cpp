6f360c5eeb148ed15d25a55ff0b694c51200cf09
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-21 14:51:09 +0530

    Time: 101 ms (38.69%), Space: 44 MB (29.56%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    void f(int idx, int prev_idx, vector<int>& nums, vector<int>& temp, set<vector<int>> &ans) {
+        if(idx == nums.size()) {
+            if(temp.size() >= 2) ans.insert(temp);
+            return;
+        }
+        
+        // not pick
+        f(idx+1, prev_idx, nums, temp, ans);
+        // pick
+        if(prev_idx == -1 || nums[idx] >= nums[prev_idx]) {
+            temp.push_back(nums[idx]);
+            f(idx+1, idx, nums, temp, ans);
+            temp.pop_back();
+        }
+    }
+    
+    
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        set<vector<int>> ans;
+        vector<int> temp;
+        f(0, -1, nums, temp, ans);
+        // convert set into 2d vector
+        vector<vector<int>> v;
+        v.assign(ans.begin(), ans.end());
+        return v;
+    }
+};
