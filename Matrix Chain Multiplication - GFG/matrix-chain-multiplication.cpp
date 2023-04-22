2d1ff3019e3c3e9f769727dddc10a7646f318a0a
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-23 01:00:16 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -39,8 +39,29 @@ public:
         // Recursion
         // return f(1, N-1, arr);
         
-        vector<vector<int>> dp(N, vector<int>(N,-1));
-        return memo(1, N-1, arr, dp);
+        // Memoization, TC: O(N^3), SC: O(N^2 + N)
+        // vector<vector<int>> dp(N, vector<int>(N,-1));
+        // return memo(1, N-1, arr, dp);
+        
+        // Tabulation, TC: O(N^3), SC: O(N^2)
+        vector<vector<int>> dp(N, vector<int>(N,0)); 
+        for(int i=1; i<N; i++) dp[i][i] = 0; // base case
+         
+        for(int i=N-1; i>=1; i--) {
+            for(int j=i+1; j<N; j++) { // i will always stay left of j
+                int mini = INT_MAX;
+                for(int k=i; k<j; k++) {
+                    int steps = arr[i-1] * arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                    if(steps < mini) mini = steps;
+                }
+                
+                dp[i][j] = mini;
+            }
+        }
+        
+        return dp[1][N-1];
+        
+        
     }
 };
 
