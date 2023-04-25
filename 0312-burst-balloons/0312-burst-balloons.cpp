810e228a970b9359144089b2419b3a3cd7be5129
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-04-25 15:26:31 +0530

    Time: 362 ms (74.24%), Space: 10.2 MB (81.76%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -33,10 +33,33 @@ public:
         // return f(1, n, nums);
         
         // Memoization, TC: O(N^3), SC: O(N^2 + N)
+        // int n = nums.size();
+        // nums.push_back(1);
+        // nums.insert(nums.begin(), 1);
+        // vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+        // return memo(1, n, nums, dp);
+        
+        // Tabulation
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        return memo(1, n, nums, dp);
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        
+        for(int i=n; i>=1; i--) {
+            for(int j=1; j<=n; j++) {
+                if(i > j) continue;
+                int maxi = INT_MIN;
+                for(int k=i; k<=j; k++) {
+                    int coins = nums[i-1] * nums[k] * nums[j+1] + dp[i][k-1] + dp[k+1][j];
+                    maxi = max(coins, maxi);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n];
+        
+        
     }
 };
