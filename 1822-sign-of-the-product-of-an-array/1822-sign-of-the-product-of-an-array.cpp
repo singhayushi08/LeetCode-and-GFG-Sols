e763a11456955a2529f97119401e858a2f49bec1
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-05-02 20:32:58 +0530

    Time: 4 ms (69.47%), Space: 10.2 MB (86.28%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int cnt_neg = 0; // count no. of negative signs in nums
+        // count no. of negative signs in nums, TC: O(n), SC: O(1)
+//         int cnt_neg = 0; 
+        
+//         for(int i=0; i<nums.size(); i++) {
+//             if(nums[i] == 0) return 0;
+//             else if(nums[i] < 0) cnt_neg++;
+//         }
+        
+//         if(cnt_neg % 2 == 0) return 1;
+//         else return -1;
+        
+        // Keep track of sign while mutlipying, TC: O(n), SC: O(1)
+        int sign = 1;
         
         for(int i=0; i<nums.size(); i++) {
             if(nums[i] == 0) return 0;
-            else if(nums[i] < 0) cnt_neg++;
+            else if(nums[i] < 0) {
+                if(sign == -1) {
+                    sign = 1;
+                } 
+                else if(sign == 1) {
+                    sign = -1;
+                }
+            }
         }
         
-        if(cnt_neg % 2 == 0) return 1;
-        else return -1;
+        return sign;
     }
 };
