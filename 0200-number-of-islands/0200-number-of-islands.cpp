a16261acc1979c0bae9bd6900fbfb8e52511a369
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-08 13:33:34 +0530

    Time: 36 ms (84.76%), Space: 13.3 MB (37.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -9,7 +9,7 @@ class Solution {
             int col = q.front().second;
             q.pop();
             
-            // check for 1 in all 4 directions
+            // check for 1 in all 4 direction neighbours
             
             // up-> row-1, col
             if(row-1 >= 0 && row-1<m && col >= 0 && col < n && grid[row-1][col] == '1' && vis[row-1][col] == 0) {
@@ -37,6 +37,22 @@ class Solution {
         }
     }
     
+    void dfs(int i, int j, vector<vector<int>> &vis, vector<vector<char>> &grid, int m, int n) {
+        if(i < 0 || i >= m || j < 0 || j >= n) { //out of bound
+            return;
+        }
+        if(grid[i][j] == '0') return; // if water, nothing to do
+        if(vis[i][j] == 1) return; // if already visited, dont visit again
+        
+        vis[i][j] = 1;
+        
+        dfs(i-1, j, vis, grid, m, n); // up
+        dfs(i, j+1, vis, grid, m, n); // right
+        dfs(i+1, j, vis, grid, m, n); // down
+        dfs(i, j-1, vis, grid, m, n); // left
+        return;
+    }
+    
 public:
     int numIslands(vector<vector<char>>& grid) {
         int cnt = 0;
@@ -49,7 +65,8 @@ public:
             for(int j=0; j<n; j++) {
                 if(grid[i][j] == '1' && vis[i][j] == 0) {
                     cnt++;
-                    bfs(i,j,vis,grid,m,n); // call bfs on node which is having 1 in grid
+                    //bfs(i,j,vis,grid,m,n); // call bfs on node which is having 1 in grid
+                    dfs(i,j,vis,grid,m,n);
                 }
             }
         }
