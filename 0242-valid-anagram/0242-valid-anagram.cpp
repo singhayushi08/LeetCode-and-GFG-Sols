3ab72b5eba6e76c6aedc148e59721d621af4c469
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-13 03:19:46 +0530

    Time: 3 ms (98.16%), Space: 7.3 MB (81.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        // TC: O(nlogn), SC: O(1)
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
+        
+        // optimal: TC: O(n), SC: O(26)
+        if(s.size() != t.size()) return false;
+        
+        int freq[26] = {0}; // for 26 characters
+        
+        for(int i=0; i<s.size(); i++) {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        
+        for(int i=0; i<26; i++) {
+            if(freq[i] != 0) return false;
+        }
+        
+        return true;
     }
 };
