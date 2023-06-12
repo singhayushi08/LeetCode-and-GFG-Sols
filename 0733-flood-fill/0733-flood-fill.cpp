aafa28eeaab60fea29a2bab1291cc504a12b535f
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-12 20:06:28 +0530

    Time: 11 ms (56.44%), Space: 14 MB (49.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -13,18 +13,38 @@ class Solution {
         dfs(i+1, j, image, starting_pixel_color, color, vis, m, n);
         dfs(i, j-1, image, starting_pixel_color, color, vis, m, n);
         
-        return;
+        return; 
+    }
+    
+    void dfs(int i, int j, vector<vector<int>>& image, int starting_pixel_color, int color, int m, int n) {
+        if(i < 0 || i >= m || j < 0 || j >= n) return; // out of bound
+        if(image[i][j] != starting_pixel_color) return; 
+        if(image[i][j] == color) return;
+        
+        image[i][j] = color;
+        
+        // now call dfs on 4 directions
+        dfs(i-1, j, image, starting_pixel_color, color, m, n);
+        dfs(i, j+1, image, starting_pixel_color, color, m, n);
+        dfs(i+1, j, image, starting_pixel_color, color, m, n);
+        dfs(i, j-1, image, starting_pixel_color, color, m, n);
+        
+        return; 
     }
     
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         if(image[sr][sc] == color) return image;
         
+        // do not alter the given image, create a copy of it
+        vector<vector<int>> ans = image;
         int m = image.size();
         int n = image[0].size();
-        vector<vector<int>> vis(m, vector<int>(n,0));
+        // vector<vector<int>> vis(m, vector<int>(n,0));
         int starting_pixel_color = image[sr][sc];
-        dfs(sr, sc, image,  starting_pixel_color, color, vis, m, n);
-        return image;
+        // dfs(sr, sc, ans,  starting_pixel_color, color, vis, m, n);
+        dfs(sr, sc, ans,  starting_pixel_color, color, m, n);
+
+        return ans;
     }
 };
