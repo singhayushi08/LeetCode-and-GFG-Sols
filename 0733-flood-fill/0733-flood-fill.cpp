475db3f9757a9988de70450eadc7538a58c01dfb
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-12 20:09:29 +0530

    Time: 14 ms (28.67%), Space: 13.9 MB (67.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -19,7 +19,7 @@ class Solution {
     void dfs(int i, int j, vector<vector<int>>& image, int starting_pixel_color, int color, int m, int n) {
         if(i < 0 || i >= m || j < 0 || j >= n) return; // out of bound
         if(image[i][j] != starting_pixel_color) return; 
-        if(image[i][j] == color) return;
+        if(image[i][j] == color) return; // if it is colored already with color, then this cell has been visited
         
         image[i][j] = color;
         
@@ -34,15 +34,27 @@ class Solution {
     
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // with visited array, TC: O(m*n*4), SC: O(2*m*n)
+//         if(image[sr][sc] == color) return image;
+        
+//         // do not alter the given image, create a copy of it
+//         vector<vector<int>> ans = image;
+//         int m = image.size();
+//         int n = image[0].size();
+//         vector<vector<int>> vis(m, vector<int>(n,0));
+//         int starting_pixel_color = image[sr][sc];
+//         dfs(sr, sc, ans,  starting_pixel_color, color, vis, m, n);
+
+//         return ans;
+        
+        // works without visited array too, TC: O(m*n*4), SC: O(m*n)
         if(image[sr][sc] == color) return image;
         
         // do not alter the given image, create a copy of it
         vector<vector<int>> ans = image;
         int m = image.size();
         int n = image[0].size();
-        // vector<vector<int>> vis(m, vector<int>(n,0));
         int starting_pixel_color = image[sr][sc];
-        // dfs(sr, sc, ans,  starting_pixel_color, color, vis, m, n);
         dfs(sr, sc, ans,  starting_pixel_color, color, m, n);
 
         return ans;
