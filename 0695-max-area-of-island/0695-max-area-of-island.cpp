0b640f68f9a4bce9f6ddb06246b3c51533fc9367
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-14 02:18:56 +0530

    Time: 23 ms (57.45%), Space: 24 MB (39.32%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis, int m, int n, int &area) {
+        if(i<0 || i>=m || j<0 || j>=n) return; // out of bound
+        if(grid[i][j] == 0) return;
+        if(vis[i][j] == 1) return; // already visited, do not visit again
+        
+        vis[i][j] = 1;
+        area++;
+        
+        // call dfs on all 4 neighbours
+        dfs(i-1, j, grid, vis, m, n, area); // up
+        dfs(i, j+1, grid, vis, m, n, area); // right
+        dfs(i+1, j, grid, vis, m, n, area); // down
+        dfs(i, j-1, grid, vis, m, n, area); // left
+        return;
+    } 
+    
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        // variation of no. of island (graph problem)
+        // TC: O(m*n), SC: O(m*n)
+        int maxi = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n,0));
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 1 && vis[i][j] == 0) {
+                    int area = 0;
+                    dfs(i,j,grid,vis,m,n,area);
+                    maxi = max(area, maxi);
+                }
+            }
+        }
+        
+        return maxi;
+    }
+};
