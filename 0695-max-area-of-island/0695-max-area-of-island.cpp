1d977aaa8b31b61d23e094ab67cfa65580d0ab3a
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2023-06-14 02:31:49 +0530

    Time: 28 ms (30.54%), Space: 23.2 MB (84.53%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -15,25 +15,59 @@ class Solution {
         return;
     } 
     
+    void dfs(int i, int j, vector<vector<int>>& grid, int m, int n, int &area) {
+        if(i<0 || i>=m || j<0 || j>=n) return; // out of bound
+        if(grid[i][j] == 0) return;
+        
+        grid[i][j] = 0; // mark it as 0, so that it does not gets visited again
+        
+        area++;
+        
+        // call dfs on all 4 neighbours
+        dfs(i-1, j, grid, m, n, area); // up
+        dfs(i, j+1, grid, m, n, area); // right
+        dfs(i+1, j, grid, m, n, area); // down
+        dfs(i, j-1, grid, m, n, area); // left
+        return;
+    }
+    
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         // variation of no. of island (graph problem)
         // TC: O(m*n), SC: O(m*n)
+//         int maxi = 0;
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> vis(m, vector<int>(n,0));
+        
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(grid[i][j] == 1 && vis[i][j] == 0) {
+//                     int area = 0;
+//                     dfs(i,j,grid,vis,m,n,area);
+//                     maxi = max(area, maxi);
+//                 }
+//             }
+//         }
+        
+//         return maxi;
+        
+        // can be done without using visited array too, TC: O(m*n), SC: O(1)
         int maxi = 0;
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int>(n,0));
         
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(grid[i][j] == 1 && vis[i][j] == 0) {
+                if(grid[i][j] == 1) {
                     int area = 0;
-                    dfs(i,j,grid,vis,m,n,area);
+                    dfs(i,j,grid,m,n,area);
                     maxi = max(area, maxi);
                 }
             }
         }
         
         return maxi;
+        
     }
 };
