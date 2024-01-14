62e59b394028747593ef2096e4efb8475db627fa
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2024-01-14 21:32:15 +0530

    Time: 7 ms (51.63%), Space: 14 MB (16.20%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence35-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divvmware-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div994-rotting-oranges/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence35-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divvmware-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div994-rotting-oranges.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence35-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divvmware-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div994-rotting-oranges/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence35-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence12-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divvmware-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div994-rotting-oranges.cpp
@@ -1,51 +1,61 @@
 class Solution {
-public:
-    int orangesRotting(vector<vector<int>>& grid) {
+    int bfs(queue<pair<int,int>> q, vector<vector<int>>& grid,vector<vector<int>>& vis, int delrow[], int delcol[]) {
+        int minTime = 0;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        // {{r,c},t} where r is row, c is col and t is time
-        queue<pair<pair<int,int>,int>> q;
+        
+        while(!q.empty()) {
+            bool checkRotten = false;
+            int size = q.size();
+            for(int i=0; i<size; i++) {
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                
+                // visit all 4 neighbours
+                for(int i=0; i<4; i++) {
+                    int nrow = row + delrow[i];
+                    int ncol = col + delcol[i];
+
+                    if(nrow >=0 && nrow < n && ncol >=0 && ncol <m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
+                        q.push({nrow,ncol});
+                        vis[nrow][ncol] = 2;
+                        checkRotten = true;
+                    }
+                }
+            }
+            if(checkRotten == true) minTime++;
+        }    
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({{i,j},0});
-                    vis[i][j] = 2; // mark rotten in visited array
-                } 
+                if(vis[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return minTime;
+    }
+    
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        vector<vector<int>> vis = grid;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(grid[i][j] == 2) { // push all rotten oranges in the queue
+                    q.push({i,j});
+                }
             }
         }
         
         int delrow[4] = {-1,0,1,0};
         int delcol[4] = {0,1,0,-1};
-        int maxTime = 0; // this max time will be the min time taken to rot all oranges
         
-        while(!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-            maxTime = max(maxTime,t);
-            
-            // traverse its neighbours
-            for(int i=0; i<4; i++) {
-                int nrow = r + delrow[i];
-                int ncol = c + delcol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && vis[nrow][ncol] != 2) { //check validity and whether it is visited in vis array
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol] = 2;
-                }
-            }
-        }
+        return bfs(q,grid,vis,delrow,delcol);
         
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j] == 1 && vis[i][j] != 2) { // if any fresh orange has not been rotted
-                    return -1;
-                }
-            }
-        }
-        
-        return maxTime;
     }
 };
