ef87be8a2d9814ad0925627d127bea34e08c578b
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2024-02-02 02:43:23 +0530

    Time: 141 ms (93.26%), Space: 119.8 MB (38.65%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2966-divide-array-into-arrays-with-max-difference.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        // TC: O(nlogn), SC: O(3)
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for(auto i: nums) cout<<i<<" ";
-        cout<<endl;
+        
         bool condn = true;
         vector<int> v;
         v.push_back(nums[0]);
@@ -15,12 +15,11 @@ public:
                 vector<int> temp;
                 v = temp;
                 v.push_back(nums[i]);
-                // for(auto i: v) cout<<i<<" ";
             }
-            else if(nums[i] - v[0] <= k && v.size() < 3) {
+            else if(nums[i] - v[0] <= k && v.size() < 3) { //logic is simple, if curr element - min element of array <= k then its fine
                 v.push_back(nums[i]);
             }
-            else if(nums[i] - v[0] > k && v.size() < 3) {
+            else if(nums[i] - v[0] > k && v.size() < 3) { //otherwise there is no point going forward coz the array is sorted so the elements in between min and curr element will always satisfy the condition but all elements diff in array should be <= k
                 condn = false;
                 break;
             }
