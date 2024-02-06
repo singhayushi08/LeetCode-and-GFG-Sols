8af4409a9daf3970cfbda9bd7d3ef6726883b6df
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2024-02-06 18:51:09 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (84.34%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1291-sequential-digits/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1291-sequential-digits.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1291-sequential-digits/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1291-sequential-digits.cpp
@@ -24,26 +24,51 @@ public:
 //         return ans;
         
         // better approach, create all sequential digits starting from 1-9 & check if they are in range
-        string str1 = to_string(low); 
-        int len1 = str1.size();
-        string str2 = to_string(high);
-        int len2 = str2.size();
+//         string str1 = to_string(low); 
+//         int len1 = str1.size();
+//         string str2 = to_string(high);
+//         int len2 = str2.size();
+//         vector<int> ans;
+        
+//         for(int digits=len1; digits<=len2; digits++) {
+//              for(int start=1; start<9; start++) { //fix the first digit and check if sequential no. can be created
+//                  if(start + digits > 10) break; //if 4 digits number has to be made and starting digit is 7, 7+4>10 cannot be made
+                 
+//                  int num = start;
+//                  int prev = start;
+//                  for(int i=0; i<digits-1; i++) {
+//                      num = num*10;
+//                      prev += 1; 
+//                      num += prev;
+//                  }
+                 
+//                  if(num >= low && num <= high) ans.push_back(num);
+//              }
+//         }
+        
+//         return ans;
+        
+        // Using queue
+        queue<int> q;
+        for(int i=1; i<9; i++) {
+            q.push(i);
+        }
         vector<int> ans;
         
-        for(int digits=len1; digits<=len2; digits++) {
-             for(int start=1; start<9; start++) { //fix the first digit and check if sequential no. can be created
-                 if(start + digits > 10) break; //if 4 digits number has to be made and starting digit is 7, 7+4>10 cannot be made
-                 
-                 int num = start;
-                 int prev = start;
-                 for(int i=0; i<digits-1; i++) {
-                     num = num*10;
-                     prev += 1; 
-                     num += prev;
-                 }
-                 
-                 if(num >= low && num <= high) ans.push_back(num);
-             }
+        while(!q.empty()){
+            int num = q.front();
+            cout<<num<<" ";
+            if(num >= low && num <= high) ans.push_back(num);
+            if(num > high) break;
+            q.pop();
+            
+            int rem = num%10;
+            if(rem < 9) {
+                num = num*10;
+                num += (rem+1);
+            
+                q.push(num);
+            }
         }
         
         return ans;
