4f11c51784221495786dfd73b8bbe7bb54cad892
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2024-05-19 18:15:45 +0530

    Time: 28 ms (85.57%), Space: 23 MB (45.24%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div947-most-stones-removed-with-same-row-or-column/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div947-most-stones-removed-with-same-row-or-column.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div947-most-stones-removed-with-same-row-or-column/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,3 +1,41 @@
+class DisjointSet{
+    public:
+    
+     vector<int> parent,size;
+    
+        DisjointSet(int n) {
+            parent.resize(n+1,0);
+            size.resize(n+1,1); //tells the no. of nodes in a component, each node is of size 1 individually
+            for(int i=0; i<=n; i++) { //indivually, each node is its parent itself
+                parent[i] = i;
+            }
+        }
+        
+        int getUltParent(int node) { // get root/ultimate parent of a node
+            if(parent[node] == node) { //base case
+                return node;
+            }
+            
+            return parent[node] = getUltParent(parent[node]); //path compression is done here
+        }
+        
+        void unionBySize(int u, int v) { // make an edge btw u and v
+            int ult_pu = getUltParent(u);
+            int ult_pv = getUltParent(v);
+            if(ult_pu == ult_pv) return;
+            
+            if(size[ult_pu] < size[ult_pv]) { //attach u to v
+                parent[ult_pu] = ult_pv;
+                size[ult_pv] += size[ult_pu];
+            } else { //size of ult_pv <= ult_pu, then attach v to u
+                parent[ult_pv] = ult_pu;
+                size[ult_pu] += size[ult_pv];
+            }
+            
+            return;
+        }
+};
+
 class Solution {
     void dfs(vector<vector<int>>& stones, int index, vector<int>& vis) {
         vis[index] = 1; //mark curr stone as visited
@@ -17,13 +55,45 @@ class Solution {
     
 public:
     int removeStones(vector<vector<int>>& stones) {
+        // Using graph DFS, TC: O(n^2), SC: O(n)
+        // int n = stones.size();
+//         vector<int> vis(n,0); //stores if curr stone (i,j) of stones array is visited or not
+        
+//         int totalComponents = 0;
+//         for(int i=0; i<n; i++) {
+//             if(vis[i] == 0) {
+//                 dfs(stones, i, vis);
+//                 totalComponents++;
+//             }
+//         }
+        
+//         return n - totalComponents;
+        
+        // Using DSU, TC: O(n+ n*4ahlpa + n*4alpha), SC: O(2n+n)
         int n = stones.size();
-        vector<int> vis(n,0);
+        int maxRow = 0;
+        int maxCol = 0;
+        for(int i=0; i<n; i++) {
+            maxRow = max(maxRow, stones[i][0]);
+            maxCol = max(maxCol, stones[i][1]);
+        }
+        
+        DisjointSet ds(maxRow + maxCol + 1);
+        
+        unordered_map<int,int> nodeStones; //stores nodes which are stones
+        for(auto it: stones) {
+            int row = it[0];
+            int col = it[1] + maxRow + 1;
+            
+            ds.unionBySize(row, col);
+            nodeStones[row] = 1;
+            nodeStones[col] = 1;
+        }
         
         int totalComponents = 0;
-        for(int i=0; i<n; i++) {
-            if(vis[i] == 0) {
-                dfs(stones, i, vis);
+        //now find unique parents of stone nodes
+        for(auto it: nodeStones) {
+            if(ds.getUltParent(it.first) == it.first) {
                 totalComponents++;
             }
         }
