7e49be7ddc50bbd4411e70a95b3446a3144c9582
Ayushi <85934392+singhayushi08@users.noreply.github.com>
2024-05-28 01:50:53 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (85.69%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1608-special-array-with-x-elements-greater-than-or-equal-x/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1608-special-array-with-x-elements-greater-than-or-equal-x/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,14 +1,27 @@
 class Solution {
+    int getFirstIndex(vector<int>& nums, int val) {
+        int low = 0;
+        int high = nums.size()-1;
+        int index = nums.size();
+        
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid] >= val) {
+                index = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return index;
+    }
 public:
     int specialArray(vector<int>& nums) {
         // Brute force, TC: O(N^2), SC: O(1)
 //         int ans = -1;
-//         int maxi = 0;
-//         for(int i=0; i<nums.size(); i++) {
-//             maxi = max(maxi, nums[i]);
-//         }
         
-//         for(int num=1; num<=maxi; num++) {
+//         for(int num=1; num<=nums.size(); num++) {
 //             int cnt = 0;
 //             for(int i=0; i<nums.size(); i++) {
 //                 if(nums[i] >= num) {
@@ -21,21 +34,35 @@ public:
 //         return ans;
         
         // Sorting, TC: O(nlogn + n^2), SC: O(1)
+//         int ans = -1;
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end());
+
+//         for(int num=1; num<=n; num++) {
+//             int cnt = 0;
+//             for(int i=0; i<nums.size(); i++) {
+//                 if(nums[i] >= num) {
+//                     cnt = (n-1)-i+1;
+//                     break;
+//                 } 
+//             }
+//             if(cnt == num) return num;
+//         }
+        
+//         return ans;
+        
+        // Sorting + binary search, TC: O(nlogn), SC: O(1)
         int ans = -1;
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        for(int num=1; num<=nums[n-1]; num++) {
-            int cnt = 0;
-            for(int i=0; i<nums.size(); i++) {
-                if(nums[i] >= num) {
-                    cnt = (n-1)-i+1;
-                    break;
-                } 
+        for(int i=1; i<=n; i++) {
+            int k = getFirstIndex(nums,i); //first index where i or greater than i is present in nums
+            if(n-k == i) {
+                return i;
             }
-            if(cnt == num) return num;
         }
         
-        return ans;
+        return -1;
     }
 };
